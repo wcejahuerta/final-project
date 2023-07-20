4c10d3b02f60157e4222fdde609931e1d6b99c08
wcejahuerta <103069876+wcejahuerta@users.noreply.github.com>
2023-07-20 07:59:31 -0400

    Update projectJS.js

--- projectJS.js
+++ projectJS.js
@@ -1,1 +1,11 @@
+function resetMealPlan() {
+    document.getElementById('email').value = '';
+    document.getElementById('name').value = '';
+    
+}
+
+function createMealPlan(){
+    var email = document.getElementById("email").value;
+    var name = documnet.getElementById("name").value;
+}
 
